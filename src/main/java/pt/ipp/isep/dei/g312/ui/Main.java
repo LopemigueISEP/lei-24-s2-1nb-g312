bb6bae351c54ae05c092c5337b43ecd37b128306
Miguel Lopes <1222183@isep.ipp.pt>
2024-05-21 23:54:10 +0100

    #30 JavaFx Implementation

--- src/main/java/pt/ipp/isep/dei/g312/ui/Main.java
+++ src/main/java/pt/ipp/isep/dei/g312/ui/Main.java
@@ -9,6 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/main/java/pt/ipp/isep/dei/g312/ui/gui/Login/LoginController.java
@@ -0,0 +1,21 @@
+package pt.ipp.isep.dei.g312.ui.gui.Login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
+
+import java.awt.*;
+
+public class LoginController {
+    @FXML
+    public PasswordField pswrd;
+    @FXML
+    private TextField userId;
+
+    @FXML
+    public void logMeIn(ActionEvent actionEvent) {
+
+    }
+}
