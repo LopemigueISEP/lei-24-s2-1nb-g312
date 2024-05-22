f2ec53b2f313c845b2cc5c69dee74ab341653bf9
Miguel Lopes <1222183@isep.ipp.pt>
2024-05-22 23:32:36 +0100

    #30 Login GUI

--- src/main/java/pt/ipp/isep/dei/g312/ui/gui/Login/LoginUI.java
+++ src/main/java/pt/ipp/isep/dei/g312/ui/gui/Login/LoginUI.java
@@ -4,18 +4,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pt.ipp.isep.dei.g312.application.controller.authorization.AuthenticationController;
+import pt.ipp.isep.dei.g312.ui.Bootstrap;
 
 import java.io.IOException;
 
-public class Login extends Application {
+public class LoginUI extends Application {
+    private final AuthenticationController ctrl;
 
     public static void main(String[] args) {
         launch();
     }
 
+    public  LoginUI(){
+        ctrl = new AuthenticationController();
+    }
+
     @Override
     public void start(Stage primaryStage) {
-        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("LoginForm.fxml"));
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.run();
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginUI.class.getResource("LoginForm.fxml"));
         Scene scene;
         try {
             scene = new Scene(fxmlLoader.load());
