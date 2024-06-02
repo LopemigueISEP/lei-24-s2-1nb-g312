ef82209f2e891e8adca8eb18dd43827142421c41
André Azevedo <1230932@isep.ipp.pt>
2024-06-02 23:18:38 +0100

    #11 #13  javafx output minor changes. update
     GSM email update bootstrap
    
     controller added table view options

--- src/main/java/pt/ipp/isep/dei/g312/application/controller/RegisterGreenSpaceController.java
+++ src/main/java/pt/ipp/isep/dei/g312/application/controller/RegisterGreenSpaceController.java
@@ -1,6 +1,7 @@
 package pt.ipp.isep.dei.g312.application.controller;
 
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import pt.ipp.isep.dei.g312.application.controller.authorization.AuthenticationController;
@@ -12,9 +13,7 @@ import pt.ipp.isep.dei.g312.repository.GreenSpaceRepository;
 import pt.ipp.isep.dei.g312.repository.Repositories;
 
 import java.util.*;
-/**
- * Controller class for registering green spaces.
- */
+
 public class RegisterGreenSpaceController {
 
     @FXML
@@ -36,16 +35,27 @@ public class RegisterGreenSpaceController {
     private Label messageLabel;
 
     @FXML
-    private ListView<String> greenSpacesListView;
+    private TableView<GreenSpace> greenSpacesTableView;
+
+    @FXML
+    private TableColumn<GreenSpace, String> nameColumn;
+
+    @FXML
+    private TableColumn<GreenSpace, String> addressColumn;
+
+    @FXML
+    private TableColumn<GreenSpace, Double> areaColumn;
+
+    @FXML
+    private TableColumn<GreenSpace, String> typologyColumn;
+
+    @FXML
+    private TableColumn<GreenSpace, String> managerColumn;
 
     private GreenSpaceRepository greenSpaceRepository;
     private AuthenticationRepository authRepository;
     private EmployeeRepository employeeRepository;
-    /**
-     * Initializes the controller.
-     *
-     * @param registerGreenSpaceUI Flag indicating whether to initialize the UI for registering green spaces or showing a list of green spaces.
-     */
+
     @FXML
     public void initialize(boolean registerGreenSpaceUI) {
         greenSpaceRepository = getGreenSpaceRepository();
@@ -57,27 +67,23 @@ public class RegisterGreenSpaceController {
             initializeShowListGreenSpacesUI();
         }
     }
-    /**
-     * Initializes the UI for registering a green space.
-     */
+
     @FXML
     public void initializeRegisterGreenSpaceUI() {
         typologyChoiceBox.setPromptText("Select Green Space Type");
         typologyChoiceBox.getItems().addAll("Garden", "Medium-Sized Park", "Large-Sized Park");
         setGreenSpaceManager();
     }
-    /**
-     * Initializes the UI for showing a list of green spaces.
-     */
+
     @FXML
     public void initializeShowListGreenSpacesUI() {
         updateGreenSpacesList();
     }
-    /**
-     * Retrieves the GreenSpaceRepository instance.
-     *
-     * @return The GreenSpaceRepository instance.
-     */
+    public RegisterGreenSpaceController(){
+        this.employeeRepository = getEmployeeRepository();
+        this.authRepository = getAuthRepository();
+        this.employeeRepository = getEmployeeRepository();
+    }
     private GreenSpaceRepository getGreenSpaceRepository() {
         if (greenSpaceRepository == null) {
             Repositories repositories = Repositories.getInstance();
@@ -85,11 +91,7 @@ public class RegisterGreenSpaceController {
         }
         return greenSpaceRepository;
     }
-    /**
-     * Retrieves the AuthenticationRepository instance.
-     *
-     * @return The AuthenticationRepository instance.
-     */
+
     private AuthenticationRepository getAuthRepository() {
         if (authRepository == null) {
             Repositories repositories = Repositories.getInstance();
@@ -97,11 +99,7 @@ public class RegisterGreenSpaceController {
         }
         return authRepository;
     }
-    /**
-     * Retrieves the EmployeeRepository instance.
-     *
-     * @return The EmployeeRepository instance.
-     */
+
     private EmployeeRepository getEmployeeRepository() {
         if (employeeRepository == null) {
             Repositories repositories = Repositories.getInstance();
@@ -109,19 +107,11 @@ public class RegisterGreenSpaceController {
         }
         return employeeRepository;
     }
-    /**
-     * Validates if the current user is logged in and has the required role for registering a green space.
-     *
-     * @return True if the current user is logged in and has the required role, false otherwise.
-     */
+
     private boolean currentUserLogInValidation() {
         return authRepository.validateUserRole(AuthenticationController.ROLE_ADMIN, AuthenticationController.ROLE_GSM);
     }
-    /**
-     * Matches the current user with the corresponding employee based on their role.
-     *
-     * @return The matched Employee object.
-     */
+
     private Employee matchEmployeeByRole() {
         try {
             String rl = authRepository.getUserRole(authRepository.getCurrentUserSession().getUserRoles());
@@ -131,19 +121,15 @@ public class RegisterGreenSpaceController {
             return null;
         }
     }
-    /**
-     * Sets the green space manager label based on the current user's role.
-     */
+
     private void setGreenSpaceManager() {
         if (currentUserLogInValidation()) {
-            greenSpaceManagerLabel.setText(Objects.requireNonNull(matchEmployeeByRole()).getName());
+            greenSpaceManagerLabel.setText(Objects.requireNonNull(matchEmployeeByRole()).getEmail());
         } else {
             greenSpaceManagerLabel.setText("Not Logged In");
         }
     }
-    /**
-     * Handles the action when the register button is clicked.
-     */
+
     public void handleRegisterButtonAction() {
         if (!validateInput()) {
             return;
@@ -183,11 +169,7 @@ public class RegisterGreenSpaceController {
             messageLabel.setText("You do not have permission to register green spaces.");
         }
     }
-    /**
-     * Validates the input fields for registering a green space.
-     *
-     * @return True if the input is valid, false otherwise.
-     */
+
     private boolean validateInput() {
         if (nameField.getText().trim().isEmpty() ||
                 addressField.getText().trim().isEmpty() ||
@@ -209,58 +191,27 @@ public class RegisterGreenSpaceController {
 
         return true;
     }
-    /**
-     * Registers a new green space with the provided details.
-     *
-     * @param name             The name of the green space.
-     * @param address          The address of the green space.
-     * @param area             The area of the green space.
-     * @param typology         The typology of the green space.
-     * @param greenSpaceManager The manager of the green space.
-     * @return An Optional containing the registered GreenSpace if successful, or empty otherwise.
-     */
+
     public Optional<GreenSpace> registerGreenSpace(String name, String address, double area, String typology, String greenSpaceManager) {
         try {
             GreenSpace greenSpace = new GreenSpace(name, address, area, typology, greenSpaceManager);
-            return Employee.registerGreenSpace(name, address, area, typology, greenSpaceManager, true);
+            return greenSpaceRepository.addGreenSpace(greenSpace);
         } catch (Exception e) {
-            System.err.println(STR."Error occurred while registering a green space: \{e.getMessage()}");
+            System.err.println("Error occurred while registering a green space: " + e.getMessage());
             return Optional.empty();
         }
     }
-    /**
-     * Updates the list view of green spaces with the latest data.
-     */
+
     private void updateGreenSpacesList() {
-        if (greenSpacesListView == null) {
+        if (greenSpacesTableView == null) {
             return;
         }
 
-        greenSpacesListView.getItems().clear();
-
-        greenSpacesListView.getItems().add(String.format("%-50s | %-50s | %-50s", "Name", "Type", "Manager"));
-
-        List<GreenSpace> greenSpaces = greenSpaceRepository.getGreenSpaceList();
-        for (GreenSpace greenSpace : greenSpaces) {
-            // Extrai os atributos relevantes e os separa por " | "
-            String name = greenSpace.getName();
-            String type = greenSpace.getTypology();
-            String manager = greenSpace.getGreenSpaceManager();
-
-            String entry = String.format("%-50s | %-50s | %-50s", name, type, manager);
-            greenSpacesListView.getItems().add(entry);
-        }
+        ObservableList<GreenSpace> greenSpaces = FXCollections.observableArrayList(greenSpaceRepository.getGreenSpaceList());
+        greenSpacesTableView.setItems(greenSpaces);
     }
-    /**
-     * Parses the area field text to a double.
-     *
-     * @param areaText The text representing the area.
-     * @return The parsed area as a double.
-     * @throws NumberFormatException If the areaText cannot be parsed to a double.
-     */
+
     private double parseAreaField(String areaText) throws NumberFormatException {
         return Double.parseDouble(areaText);
     }
-
-
 }
--- src/main/java/pt/ipp/isep/dei/g312/ui/Bootstrap.java
+++ src/main/java/pt/ipp/isep/dei/g312/ui/Bootstrap.java
@@ -57,7 +57,7 @@ public class Bootstrap implements Runnable {
             employeeRepository.addEmployee(new Employee("Employee",  dateFormat.parse("01/01/1950"), "employee@this.app", 919017113,  dateFormat.parse("01/01/1968"), "246597858", "Porto", "12345678", "EMPLOYEE"));
             employeeRepository.addEmployee(new Employee("Human Resources Manager",  dateFormat.parse("01/01/1950"), "HRM@this.app", 919017113,  dateFormat.parse("01/01/1968"), "246597857", "Porto", "12345678", "HRM"));
             employeeRepository.addEmployee(new Employee("Vehicle and Equipment Fleet Manager", dateFormat.parse("01/01/1950"), "VFM@this.app", 919017113,  dateFormat.parse("01/01/1968"), "246597856", "Porto", "12345678", "VFM"));
-            employeeRepository.addEmployee(new Employee("Green Space Manager",  dateFormat.parse("01/01/1950"), "GSM1@this.app", 919017113,  dateFormat.parse("01/01/1968"), "246597855", "Porto", "12345678", "GSM"));
+            employeeRepository.addEmployee(new Employee("Green Space Manager",  dateFormat.parse("01/01/1950"), "GSM@this.app", 919017113,  dateFormat.parse("01/01/1968"), "246597855", "Porto", "12345678", "GSM"));
 
         } catch (ParseException e) {
             Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, "Incorrect date Format", e);
