6b887c1dccf573cc706cd2dfab1f58a37d55fa45
RuiGRMargarido <1230420@isep.ipp.pt>
2024-06-06 02:06:15 +0100

    US27 - added more tests #18

--- src/test/java/pt/ipp/isep/dei/g312/repository/GreenSpaceRepositoryTest.java
+++ src/test/java/pt/ipp/isep/dei/g312/repository/GreenSpaceRepositoryTest.java
@@ -82,13 +82,22 @@ class GreenSpaceRepositoryTest {
     @Test
     void getGreenSpaceManagedByMe() {
         GreenSpaceRepository repository = new GreenSpaceRepository();
+        GreenSpace greenSpace3 = new GreenSpace("Parque Eduardo VII", "Lisboa", 25.0, "Urban Park", "Luís Montenegro");
         GreenSpace greenSpace2 = new GreenSpace("Passadiços do Paiva", "Arouca", 396.5, "Medium-Sized Park", "Marcelo");
         GreenSpace greenSpace1 = new GreenSpace("Peneda/Gerês", "Minho", 965.0, "Large-Sized Park", "Luís Montenegro");
         repository.addGreenSpace(greenSpace1);
         repository.addGreenSpace(greenSpace2);
+        repository.addGreenSpace(greenSpace3);
 
-        assertEquals(repository.getGreenSpaceManagedByMe("Marcelo").getFirst(),greenSpace2);
-        assertNotEquals(repository.getGreenSpaceManagedByMe("Luís Montenegro").getFirst(),greenSpace2);
+        assertEquals(repository.getGreenSpaceManagedByMe("Marcelo").getFirst(),greenSpace2); //Teste 1 - verifica se devolve o greenSpace correto
+        assertNotEquals(repository.getGreenSpaceManagedByMe("Luís Montenegro").getFirst(),greenSpace2); //Teste 2 - verifica se não devolve o greenspace gerido por outro ator
 
+        // Teste 3 - Verifica se devolve múltiplos greenSpaces geridos pelo mesmo ator
+        List<GreenSpace> greenSpacesLuis = repository.getGreenSpaceManagedByMe("Luís Montenegro");
+        assertTrue(greenSpacesLuis.contains(greenSpace1));
+        assertTrue(greenSpacesLuis.contains(greenSpace3));
+
+        // Teste 4 - Verifica se devolve uma lista vazia quando o usuário não gere nenhum espaço verde
+        assertTrue(repository.getGreenSpaceManagedByMe("João").isEmpty());
     }
 }
